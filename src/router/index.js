b3639e9d38ecdbdc07a208fc3b3b5b26feeeb0db
Juan <jebalseroo@unal.edu.co>
2020-09-28 21:26:27 -0500

    se genero la maquetacion del Landing | FRT-9 |

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/QuienesSomos',
+    name: 'QuienesSomos',
+    component: () => import('../views/QuienesSomos.vue')
+  },
+  {
+    path: '/Contacto',
+    name: 'Contacto',
+    component: () => import('../views/Contacto.vue')
+  },
+  {
+    path: '/Servicios',
+    name: 'Servicios',
+    component: () => import('../views/Servicios.vue')
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
---
+++ src/views/Contacto.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Contacto">
+    <h1>This is an Contacto page</h1>
+  </div>
+</template>
