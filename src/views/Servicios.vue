29c1d3e398e63bc03ec7d831858a007e24088cac
ccrojast <56688496+ccrojast@users.noreply.github.com>
2020-10-02 16:31:53 -0500

    prueba

--- src/views/Servicios.vue
+++ src/views/Servicios.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="Servicios">
-    <h1>This is an Servicios page</h1>
+    <head>
+  <link rel="stylesheet" href="/static/css/bootstrap.css">
+</head>
+    <body>
+    <h1>Nuestros Servicios</h1>
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="C:\Users\qwe\Desktop\servicios" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
+    </body>
   </div>
 </template>
