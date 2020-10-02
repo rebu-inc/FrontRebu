64dcb8da777e2402f48ad8e20efcc315854529b7
ccrojast <ccrojast@unal.edu.co>
2020-10-02 16:45:41 -0500

    prueba2

--- src/views/Servicios.vue
+++ src/views/Servicios.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="Servicios">
-    <head>
-  <link rel="stylesheet" href="/static/css/bootstrap.css">
-</head>
     <body>
     <h1>Nuestros Servicios</h1>
-    <div id="carousel" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="C:\Users\qwe\Desktop\servicios" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-</div>
     </body>
   </div>
 </template>
