659f1ed209f0022ab881ec990a277829d6c0ef7c
Juan <jebalseroo@unal.edu.co>
2020-10-15 19:09:47 -0500

    arreglo de vistas en router

--- src/router/index.js
+++ src/router/index.js
@@ -8,37 +8,62 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/Inicio',
+        name: 'Inicio',
+        component: () => import('../views/Inicio.vue')
+      },
+      {
+        path: '/QuienesSomos',
+        name: 'QuienesSomos',
+        component: () => import('../views/QuienesSomos.vue')
+      },
+      {
+        path: '/Contacto',
+        name: 'Contacto',
+        component: () => import('../views/Contacto.vue')
+      },
+      {
+        path: '/Servicios',
+        name: 'Servicios',
+        component: () => import('../views/Servicios.vue')
+      },
+      {
+        path: '/Login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: '/Registro',
+        name: 'Registro',
+        component: () => import('../views/Registro.vue')
+      }
+    ]
   },
   {
-    path: '/QuienesSomos',
-    name: 'QuienesSomos',
-    component: () => import('../views/QuienesSomos.vue')
-  },
-  {
-    path: '/Contacto',
-    name: 'Contacto',
-    component: () => import('../views/Contacto.vue')
-  },
-  {
-    path: '/Servicios',
-    name: 'Servicios',
-    component: () => import('../views/Servicios.vue')
-  },
-  {
-    path: '/Login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/Registro',
-    name: 'Registro',
-    component: () => import('../views/Registro.vue')
-  },
-  {
-    path: '/Inicio-operador',
-    name: 'Inicio-operador',
-    component: () => import('../views/Inicio-operador.vue')
+    path: '/Landing',
+    name: 'Landing',
+    component: () => import('../views/Landing.vue'),
+    children: [
+      {
+        path: '/Landing-operador',
+        name: 'Landing-operador',
+        component: () => import('../views/Landing-operador.vue')
+      },
+      {
+        path: '/Landing-Administrador',
+        name: 'Landing-Administrador',
+        component: () => import('../views/Landing-Administrador.vue')
+      },
+      {
+        path: '/Landing-Cliente',
+        name: 'Landing-Cliente',
+        component: () => import('../views/Landing-Cliente.vue')
+      }
+
+    ]
   }
 ]
 const router = new VueRouter({
