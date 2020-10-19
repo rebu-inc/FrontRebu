4e5edad292dd11417a3319875884237a2d55b31a
ccrojast <56688496+ccrojast@users.noreply.github.com>
2020-10-18 22:51:27 -0500

    Cambios vista servicios.

--- src/views/Servicios.vue
+++ src/views/Servicios.vue
@@ -4,10 +4,9 @@
      <b-row>
      <h1></h1>
        <b-col class="pt-3 pr-5">
-         <h2 class="text-justify font-weight-bold ">
+         <h1 class="text-justify font-weight-bold text-right ">
           En TicketWork ofrecemos servicios que te brindan una mejor solucion a tus necesidades.
-          Con TicketCode puedes mantener un seguiminto de los servicios prestados por su empresa, y los operadores destinados a realizarlas, esto mediante un moderno sistema de tickets que le permitira gestionar de forma eficiente todos sus procesos.
-         </h2>
+         </h1>
       </b-col>
        <b-col>
          <img src="../assets/serv2.jpg" style="width:800px" height="500" alt="holi">
