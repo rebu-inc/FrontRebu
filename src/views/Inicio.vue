bc7e30b40c5a83d2e62dfdf413f1079710aad8a1
ccrojast <56688496+ccrojast@users.noreply.github.com>
2020-10-20 21:06:00 -0500

    Vista Inicio

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="Servicios">
-    <h1>This is an inicio page</h1>
+  <div class="Inicio">
+    <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="2000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="720"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="720"
+            height="480"
+            src="../assets/L3.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="720"
+            height="480"
+            src="../assets/L2.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="720"
+            height="480"
+            src="../assets/L1.jpg"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+    </b-carousel>
+
+    <p class="mt-4">
+      Slide #: {{ slide }}<br>
+      Sliding: {{ sliding }}
+    </p>
+  </div>
   </div>
 </template>
+
+<script>
+export default {
+  data (a) {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    }
+  }
+}
+</script>
